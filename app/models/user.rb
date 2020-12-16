fed48b39dc5c6ede0f49be04c6d2208eff0cca22
Hakushin0318 <1997hironobu@gmail.com>
2020-12-16 20:47:37 +0900

    path設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i }  
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :last_name_ruby, presence: true, format: { with: /\A[ァ-ヶ々]+\z/ }
+  validates :first_name_ruby, presence: true, format: { with: /\A[ァ-ヶ々]+\z/ }
+  validates :birthday, presence: true
 end
