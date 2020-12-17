9d5f8e5ce3a4b32b7298c5594ce6a5b2673fd6ae
Hakushin0318 <1997hironobu@gmail.com>
2020-12-17 12:48:47 +0900

    Userモデル単体テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-
-
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i }  
+  validates :email, uniqueness: true
+  validates :password, format: { with: /\A[a-z0-9]+\z/i }  
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
   validates :last_name_ruby, presence: true, format: { with: /\A[ァ-ヶ々]+\z/ }
