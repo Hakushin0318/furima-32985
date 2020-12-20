5efc3259a773c3ed20263dff8e50f855eb2e114e
Hakushin0318 <1997hironobu@gmail.com>
2020-12-20 13:21:12 +0900

    商品情報編集機能の編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -23,9 +25,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :details, :category_id, :state_id, :delivery_fee_id, :delivery_source_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
