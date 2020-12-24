8220087412d8495498a06d35d12c3b4348131931
Hakushin0318 <1997hironobu@gmail.com>
2020-12-24 10:52:58 +0900

    建物のダミーデータの追加、メソッド化、不要部分の削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :item_find, only: [:index, :create]
   before_action :move_to_index
+  before_action :pay_item, only: [:create]
 
   def index
     @order_address = OrderAddress.new
@@ -10,12 +11,6 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
       @order_address.save
       redirect_to root_path
     else
@@ -26,7 +21,7 @@ class OrdersController < ApplicationController
   private
 
     def order_params
-      params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :price).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+      params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
     end
   
     def move_to_index
@@ -37,6 +32,15 @@ class OrdersController < ApplicationController
 
     def item_find
       @item = Item.find(params[:item_id])
+    end
+
+    def pai_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
     end
 end
 
--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefecture_id { 2 }
     city { Faker::Lorem.sentence }
     address { Faker::Lorem.sentence }
+    building_name { Faker::Lorem.sentence }
     phone_number { "11111111111" }
     token {"tok_abcdefghijk00000000000000000"}
   end
